42414b5d58e90664327e9bc3630253e46cd6e811
Daniel Reder <danielreder5@gmail.com>
2023-12-18 22:38:03 -0600

    Moved files around, added images to image slideshow, added styling

--- JS/index.js
+++ JS/index.js
@@ -1,2 +1,19 @@
 document.getElementById('date').innerHTML = new Date().toDateString();
 // this tells the html file to create a variable called date and sets it to the current date 
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
